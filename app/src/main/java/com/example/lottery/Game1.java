c9e287fd7ea9ce6ba75198ff4019eb6e84c3e983
Amritpal Singh <aamritpa@sfu.ca>
2020-10-27 02:02:58 -0700

    Game 1 on click color changed working

--- app/src/main/java/com/example/lottery/Game1.java
+++ app/src/main/java/com/example/lottery/Game1.java
@@ -2,7 +2,9 @@ package com.example.lottery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class Game1 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Game1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game1);
+    }
+
+    public void chooseNumber(View view){
+        view.setBackgroundColor(Color.parseColor("#455C5A"));
     }
 }
